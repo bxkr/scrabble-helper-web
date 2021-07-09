8306092f3a99e9e5fb3c027f547957f146665be9
Andrew <bxkr@mail.com>
2021-07-09 19:39:50 +0300

    Simple table logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {numberRange} from "../scripts/range";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'scrabble-helper';
+  title = 'scrabble-helper'
+  row_range = numberRange(1, 16)
+  col_range(rn: number, cn: number): string {
+    const al = Array.from(String.fromCharCode(...[...Array('О'.charCodeAt(0) - 'А'.charCodeAt(0) + 1).keys()].map(i => i + 'А'.charCodeAt(0))))
+    const nl = numberRange(1, 16)
+    return al[cn-1] + nl[rn-1].toString()
+  }
 }
---
+++ src/scripts/range.ts
@@ -0,0 +1,3 @@
+export function numberRange(start: number, end: number): number[] {
+  return new Array(end - start).fill(0).map((d, i) => i + start)
+}
