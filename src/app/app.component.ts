dd0607dc0aeea56f881240ca1b302e0a684b153f
Andrew <bxkr@mail.com>
2021-07-12 23:26:20 +0300

    Direction of input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,8 @@ export class AppComponent implements AfterViewInit {
   row_range = numberRange(1, 16)
   clicked_cells: any[] = []
   setting_word = false
+  direction: string | undefined;
+  dir_ref: string | undefined;
   alpha_arr = Array.from(String.fromCharCode(...[...Array('О'.charCodeAt(0) - 'А'.charCodeAt(0) + 1).keys()].map(i => i + 'А'.charCodeAt(0))))
   col_range(rn: number, cn: number): string {
     const nl = numberRange(1, 16)
@@ -36,8 +38,19 @@ export class AppComponent implements AfterViewInit {
 
   public onCellTouch(cell: any): void {
     this.setting_word = true
-    this.clicked_cells.push(cell.target)
-    cell.target.style.borderColor = 'red'
+    if (this.direction == undefined || cell.target.id[0] == this.dir_ref || cell.target.id[1] == this.dir_ref) {
+      this.clicked_cells.push(cell.target)
+      cell.target.style.borderColor = 'red'
+    }
+    if (this.direction == undefined && this.clicked_cells.length == 2) {
+      if (this.clicked_cells[0].id[0] == this.clicked_cells[1].id[0]) {
+        this.direction = 'v'
+        this.dir_ref = this.clicked_cells[0].id[0]
+      } else if (this.clicked_cells[0].id[1] == this.clicked_cells[1].id[1]) {
+        this.direction = 'h'
+        this.dir_ref = this.clicked_cells[0].id[1]
+      }
+    }
   }
 
   public clearClicked(): void {
@@ -46,5 +59,7 @@ export class AppComponent implements AfterViewInit {
     })
     this.clicked_cells = []
     this.setting_word = false
+    this.direction = undefined
+    this.dir_ref = undefined
   }
 }
