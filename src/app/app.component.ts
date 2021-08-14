9e7f7787d12041a3b2f26a84ce404d13b401a30d
Andrew <bxkr@mail.com>
2021-08-15 00:00:10 +0300

    New animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,6 +24,20 @@ interface CellAny {
       transition(':enter', [style({ opacity: 0 }), animate('.5s ease-out', style({ opacity: 1 }))]),
       transition(':leave', [style({ opacity: 1 }), animate('.5s ease-in', style({ opacity: 0 }))]),
     ]),
+    trigger('slideUp', [
+      transition(':enter', [
+        style({ transform: 'translateY(-300%)' }),
+        animate('.5s ease-in', style({ transform: 'translateY(0%)' })),
+      ]),
+      transition(':leave', [animate('.5s ease-in', style({ transform: 'translateY(100%)' }))]),
+    ]),
+    trigger('slideDown', [
+      transition(':enter', [
+        style({ transform: 'translateY(100%)' }),
+        animate('.5s ease-in', style({ transform: 'translateY(0%)' })),
+      ]),
+      transition(':leave', [animate('.5s ease-in', style({ transform: 'translateY(100%)' }))]),
+    ]),
   ],
 })
 export class AppComponent implements OnInit {
